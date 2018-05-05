3261e1f9b1b39096afaa03e2abc8851430e08c6c
mark <murimimg180@gmail.com>
2018-05-06 00:51:33 +0300

    make migrations

--- gallery/images/tests.py
+++ gallery/images/tests.py
@@ -6,3 +6,7 @@ class EditorTestClass(TestCase):
     # Set up method
     def setUp(self):
         self.Ramza= Editor(first_name = 'Ramza', last_name ='Reseni', email ='Ramza@moringaschool.com')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.james,Editor))
