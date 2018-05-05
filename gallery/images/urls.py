9944cf4d6adc5bc353355ac6595a66fc22ea8876
mark <murimimg180@gmail.com>
2018-05-05 21:54:41 +0300

    made changes to the views

--- gallery/images/urls.py
+++ gallery/images/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.images_of_day,name='imagesToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_images,name = 'pastImages'
-        views.past_days_images, name ='pastImages'),
+    url(r'^$',views.images_of_day,name='imagesToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_images,name = 'pastImages'),
 ]
 
 if settings.DEBUG:
