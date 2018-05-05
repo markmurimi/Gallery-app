3f994517a0dc372df23df4c49a9f977baf2fc2c9
mark <murimimg180@gmail.com>
2018-05-05 21:36:46 +0300

    made changes

--- gallery/images/urls.py
+++ gallery/images/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.images_of_day,name='imagesToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_images,name = 'pastImages')
+    url('^$',views.images_of_day,name='imagesToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_images,name = 'pastImages'
+        views.past_days_images, name ='pastImages'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
