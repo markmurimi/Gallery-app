c91d3c47a6efd480981becb597e2826c682d52e9
mark <murimimg180@gmail.com>
2018-05-06 01:20:34 +0300

    defining different tests

--- gallery/images/tests.py
+++ gallery/images/tests.py
@@ -16,3 +16,24 @@ class EditorTestClass(TestCase):
         self.Ramza.save_editor()
         editors = Editor.objects.all()
         self.assertTrue(len(editors) > 0)
+
+class PostTestClass(TestCase):
+
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.Ramza= Editor(first_name = 'Ramza', last_name ='Reseni', email ='Ramza@moringaschool.com')
+        self.Ramza.save_editor()
+
+        # Creating a new tag and saving it
+        self.new_tag = tags(name = 'testing')
+        self.new_tag.save()
+
+        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.Ramza)
+        self.new_article.save()
+
+        self.new_article.tags.add(self.new_tag)
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        tags.objects.all().delete()
+        Post.objects.all().delete()
