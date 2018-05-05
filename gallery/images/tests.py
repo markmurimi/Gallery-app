f941be96c4cded9ab1221969ea95bfc2ef16dd27
mark <murimimg180@gmail.com>
2018-05-06 00:49:36 +0300

    added models and tests

--- gallery/images/tests.py
+++ gallery/images/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
-
+from .models import Editor,Post,tags
 # Create your tests here.
+class EditorTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.Ramza= Editor(first_name = 'Ramza', last_name ='Reseni', email ='Ramza@moringaschool.com')
