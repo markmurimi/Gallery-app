02e9f7e75a4148550e8525468ad333b67756fabb
mark <murimimg180@gmail.com>
2018-05-06 01:23:29 +0300

    defined new class methods

--- gallery/images/models.py
+++ gallery/images/models.py
@@ -26,3 +26,9 @@ class Post(models.Model):
     editor = models.ForeignKey(Editor)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
+
+@classmethod
+    def todays_news(cls):
+        today = dt.date.today()
+        news = cls.objects.filter(pub_date__date = today)
+        return news
