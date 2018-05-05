150068dbbfbee8230e14d796e2d2b600629a740f
mark <murimimg180@gmail.com>
2018-05-05 16:08:31 +0300

    modified files

--- gallery/images/urls.py
+++ gallery/images/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.images_of_day,name='imagesToday')
+    url('^$',views.images_of_day,name='imagesToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_images,name = 'pastImages')
 ]
--- gallery/images/views.py
+++ gallery/images/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime as dt
-
+from django.http  import HttpResponse,Http404
 # Create your views here.
+
+def welcome(request):
+    return render(request, 'welcome.html')
+
 def images_of_day(request):
     date = dt.date.today() 
     day = convert_dates(date)
@@ -25,3 +29,21 @@ def convert_dates(dates):
     # Returning the actual day of the week
     day = days[day_number]
     return day
+
+def past_days_images(request,past_date):
+        # Converts data from the string Url
+    try:
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        raise Http404()
+
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>Images for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
