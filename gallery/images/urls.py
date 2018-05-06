a93ebaa2003822a40c0210b2b08c5b87663e1353
mark <murimimg180@gmail.com>
2018-05-06 10:04:02 +0300

    added search bar

--- gallery/images/urls.py
+++ gallery/images/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns=[
     url(r'^$',views.images_today,name='imagesToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_images,name = 'pastImages'),
+    url(r'^search/', views.search_results, name='search_results')
 ]
 
